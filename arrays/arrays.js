0f5db474bb6c91b78888e36146b31123ac91e6d5
Brandon <70345941@continental.edu.pe>
2022-11-23 16:54:49 -0500

    shift y unshift

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -12,23 +12,40 @@ class MyArray{
     return this.data;
   }
   pop() {
+    // Delete the last element
     const lastItem = this.data[this.length - 1];
     delete this.data[this.length - 1];
     this.length--;
     return lastItem;
   }
   deleteItem(index) {
+    // Delete a specific element
     const item = this.data[index];
     this.shiftIndex(index);
     return item;
   }
   shiftIndex(index) {
+    // Delete a specific element
     for (let i = index; i < this.length; i++){
       this.data[i] = this.data[i + 1];
     }
     delete this.data[this.length - 1];
     this.length--;
-    return this.data;
+  }
+  unshift(item) {
+    // Adds an element to the beginning of the array
+    for (let i = this.length; i > 0; i--){
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return item;
+  }
+  shift() {
+    // Removes the first element of the array
+    const firstItem = this.data[0];
+    this.shiftIndex(0);
+    return firstItem;
   }
 }
 
@@ -44,5 +61,10 @@ console.log('================== POP');
 console.log(arr.pop());
 console.log('================== DELETE ITEM');
 console.log(arr.deleteItem(1));
+console.log('================== UNSHIFT');
+console.log(arr.unshift('juana'));
+console.log(arr.unshift('marlon'));
+console.log('================== SHIFT');
+console.log(arr.shift());
 console.log('================== ARRAY');
 console.log(arr);
